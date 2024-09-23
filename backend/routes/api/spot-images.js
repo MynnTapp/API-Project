5267492db379599695004aa130ce8cb9f2d969fc
MynnTapp <Desmynnmjc@gmail.com>
2024-09-23 15:05:55 -0500

    rearranged the files. utils has auth and validation in them

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.post("/api/spots/:spotId/images", (res, req) => {
+  const spotId = req.params.spotId;
+  const image = req.body;
+  const db = req.db;
+  db.collection("spots").updateOne(
+    { _id: spotId },
+    {
+      $push: {
+        images: image,
+      },
+    },
+    (err) => {
+      if (err) {
+        res.status(500).send({ message: "Error updating spot" });
+      } else {
+        res.send({ message: "Spot updated successfully" });
+      }
+    }
+  );
+});
