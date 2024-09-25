93dc96a37ff7f69ac8ca8329ce1fa77cb9d860ce
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-09-25 10:51:53 -0700

    Removed password error message and updated successful statuscode for Users

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -13,7 +13,7 @@ const validateSignup = [
   check("email").exists({ checkFalsy: true }).isEmail().withMessage("Please provide a valid email."),
   check("username").exists({ checkFalsy: true }).isLength({ min: 4 }).withMessage("Please provide a username with at least 4 characters."),
   check("username").not().isEmail().withMessage("Username cannot be an email."),
-  check("password").exists({ checkFalsy: true }).isLength({ min: 6 }).withMessage("Password must be 6 characters or more."),
+  // check("password").exists({ checkFalsy: true }).isLength({ min: 6 }).withMessage("Password must be 6 characters or more."),
   check("firstName").exists({ checkFalsy: true }).withMessage("First Name is required."),
   check("lastName").exists({ checkFalsy: true }).withMessage("Last Name is required."),
   handleValidationErrors,
@@ -35,7 +35,7 @@ router.post("/", validateSignup, async (req, res) => {
 
   await setTokenCookie(res, safeUser);
 
-  return res.json({
+  return res.status(201).json({
     user: safeUser,
   });
 });
