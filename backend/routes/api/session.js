6c08f5b42a4cc5e1f645a7be4934507a24bda4cf
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-09-25 11:11:40 -0700

    Edited error messages for Login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -44,10 +44,10 @@ router.post("/", validateLogin, async (req, res, next) => {
   });
 
   if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-    const err = new Error("Login failed");
+    const err = new Error("Invalid credentials");
     err.status = 401;
-    err.title = "Login failed";
-    err.errors = { credential: "The provided credentials were invalid." };
+    // err.title = "Login failed";
+    // err.errors = { credential: "The provided credentials were invalid." };
     return next(err);
   }
 
