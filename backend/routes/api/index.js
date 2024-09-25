b6a2b41337c3156ad4af7c5144f93bb0985efe3a
MynnTapp <Desmynnmjc@gmail.com>
2024-09-25 10:25:15 -0500

    added the rest of the routes to index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,11 @@ const router = require("express").Router();
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const { restoreUser } = require("../../utils/auth.js");
-
+const spots = require("./spots.js");
+const spotImages = require("./spot-images.js");
+const review = require("./reviews.js");
+const reviewImages = require("./review-images.js");
+const booking = require("./bookings.js");
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
@@ -12,6 +16,16 @@ router.use(restoreUser);
 router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
+
+router.use("/spots", spots);
+
+router.use("/spot-images", spotImages);
+
+router.use("/reviews", review);
+
+router.use("/review-images", reviewImages);
+
+router.use("/bookings", booking);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
